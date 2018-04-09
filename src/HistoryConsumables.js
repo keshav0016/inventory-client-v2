8073b906fa123551f17bb6144f44ff1d0428f409
EmmanuelCBaby <emmanuel.b@westagilelabs.com>
2018-04-09 18:31:51 +0530

    changed the consumable history UI

--- src/HistoryConsumables.js
+++ src/HistoryConsumables.js
@@ -42,8 +42,8 @@ class HistoryConsumables extends Component{
                <Table centered striped>
                    <thead>
                        <tr>
-                           <th data-field="consumable id">Consumable Id</th>
-                           <th data-field="user id">User Id</th>
+                           <th data-field="consumable id">Consumable Name</th>
+                           <th data-field="user id">Employee Name</th>
                            <th data-field="ticket number">Ticket Number</th>
                            <th data-field="assigned date">Assigned Date</th>
                            <th data-field="assigned quantity">Assigned Quantity</th>
@@ -61,8 +61,8 @@ class HistoryConsumables extends Component{
                    <tbody>
                        {this.state.history.map((consumable, index) => {
                            return <tr key={index}>
-                           <td>{consumable.consumable_id ? consumable.consumable_id : '-'}</td>
-                           <td>{consumable.user_id ? consumable.user_id : '-'}</td>
+                           <td>{consumable.consumable.name ? consumable.consumable.name : '-'}</td>
+                           <td>{consumable.user ? consumable.user.first_name + " " + consumable.user.last_name : '-'}</td>
                            <td>{consumable.user_id ? (consumable.ticket_number ? consumable.ticket_number : '(Assigned by Admin)') : '-'}</td>
                            <td>{consumable.assigned_date ? moment(consumable.assigned_date).format('DD MMM YYYY') : '-'}</td>
                            <td>{consumable.assigned_date ? consumable.quantity : '-'}</td>
