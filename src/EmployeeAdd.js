4905cfc1f7253c4914487e688b68d98f072d93c5
mulakalaveena <m.veena2k14@gmail.com>
2018-04-13 16:49:02 +0530

    changes made in employee

--- src/EmployeeAdd.js
+++ src/EmployeeAdd.js
@@ -8,14 +8,42 @@ class EmployeeAdd extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      first_name: '',
-      last_name: '',
-      password: '',
-      age: '',
-      gender: '',
-      department: 'HR',
-      designation: '',
-      user_id: ''
+      first_name: {
+        value: "",
+        showError: false,
+        error: "",
+      },
+      last_name: {
+        value: "",
+        showError: false,
+        error: "",
+      },
+      age: {
+        value: "",
+        showError: false,
+        error: "",
+      },
+      gender: {
+        value: "",
+        showError: false,
+        error: "",
+      },
+      department: {
+        value: "HR",
+        showError: false,
+        error: "",
+      },
+      designation: {
+        value: "",
+        showError: false,
+        error: "",
+      },
+      user_id: {
+        value: "",
+        showError: false,
+        error: "",
+      },
+    
     }
     this.handleCreate = this.handleCreate.bind(this)
     this.handleAge = this.handleAge.bind(this)
@@ -29,43 +57,117 @@ class EmployeeAdd extends Component {
   }
   componentDidMount(){
     $('label').addClass('active')
-}
+  }
   handleCreate(){
-    if(!this.state.user_id || !this.state.first_name || !this.state.last_name || !this.state.age || !this.state.gender || !this.state.designation || !this.state.department){
-      window.Materialize.toast('All the * marked fields are required', 4000)
-    }else
-    {
+    if (!this.state.user_id.value){
+      this.setState({
+        user_id: Object.assign(this.state.user_id, {
+            error: "Employee Id is required",
+            showError: true,
+        })
+      })
+    } if( !this.state.first_name.value ){
+      this.setState({
+        first_name: Object.assign(this.state.first_name, {
+          error: "First name  is required",
+          showError: true,
+        })
+      })
+    }if(!this.state.last_name.value){
+      this.setState({
+        last_name: Object.assign(this.state.last_name, {
+          error: "Last name  is required",
+          showError: true,
+        }),
+      })
+    }if(!this.state.gender.value){
+      this.setState({
+        gender: Object.assign(this.state.gender, {
+          error: "Gender  is required",
+          showError: true,
+        }),
+      })
+    }if(!this.state.age.value) {
+      this.setState({
+        age: Object.assign(this.state.age, {
+          error: "Age  is required",
+          showError: true,
+        })
+      })
+    }if(!this.state.department.value){
+      this.setState({
+        department: Object.assign(this.state.department, {
+          error: "Department is required",
+          showError: true,
+        }),
+      })
+    }if(!this.state.designation.value){
+      this.setState({
+        designation: Object.assign(this.state.designation, {
+          error: "Designation is required",
+          showError: true,
+        }),
+      })
+    }
+    else{
       axios({
         method: 'post',
         url: 'http://localhost:3001/employees/create',
         data:{
-          user_id: this.state.user_id,
-          first_name:this.state.first_name,
-          last_name:this.state.last_name,
-          password:this.state.password,
-          age:this.state.age,
-          gender: this.state.gender,
-          department:this.state.department,
-          designation:this.state.designation,
+          user_id: this.state.user_id.value,
+          first_name:this.state.first_name.value,
+          last_name:this.state.last_name.value,
+          age:this.state.age.value,
+          gender: this.state.gender.value,
+          department:this.state.department.value,
+          designation:this.state.designation.value,
         },
         withCredentials: true
       })
       .then((res)=>{
         if(res.data.message === 'employee created'){
           this.setState({
-            user_id: '',
-            first_name: '',
-            last_name: '',
-            age: '',
-            gender: '',
-            department: '',
-            designation: '',
+            first_name: {
+              value: "",
+              showError: false,
+              error: "",
+            },
+            last_name: {
+              value: "",
+              showError: false,
+              error: "",
+            },
+            age: {
+              value: "",
+              showError: false,
+              error: "",
+            },
+            gender: {
+              value: "",
+              showError: false,
+              error: "",
+            },
+            department: {
+              value: "HR",
+              showError: false,
+              error: "",
+            },
+            designation: {
+              value: "",
+              showError: false,
+              error: "",
+            },
+            user_id: {
+              value: "",
+              showError: false,
+              error: "",
+            },
           })
           window.Materialize.toast('Employee added', 4000)
           // this.props.setHandleListRequest(true)
         }else if(res.data.message ==='user exists'){
           window.Materialize.toast('user already exists',3000)
-
+  
         
         }else if(res.data.error[0].message ==='first name should be alphabets'){
           window.Materialize.toast('firstname should be filled and should be only letters',3000)
@@ -77,51 +179,58 @@ class EmployeeAdd extends Component {
       })
     }
     
-   
   }
   handleFirstname(e){
     this.setState({
-    first_name: e.target.value
+      first_name: Object.assign(this.state.first_name, {
+        value: e.target.value
+      })  
       
     })
   }
   handleLastname(e){
     this.setState({
-    last_name: e.target.value
-      
+      last_name: Object.assign(this.state.last_name, {
+        value: e.target.value
+      })  
     })
   }
   handleAge(e){
     this.setState({
-    age: e.target.value
+      age: Object.assign(this.state.age, {
+        value: e.target.value
+      })  
       
-    })
-  }
-  handlePassword(e){
-    this.setState({
-      password: e.target.value
     })
   }
   handleDepartment(e){
     this.setState({
-    department: e.target.value
-      
+      department: Object.assign(this.state.department, {
+        value: e.target.value
+      })  
+        
     })
   }
   handleDesignation(e){
     this.setState({
-    designation: e.target.value
+      designation: Object.assign(this.state.designation, {
+        value: e.target.value
+      })  
       
     })
   }
   handleGender(e){
     this.setState({
-      gender: e.target.value
+      gender: Object.assign(this.state.gender, {
+        value: e.target.value
+      })  
     })
   }
   handleUser_Id(e){
     this.setState({
-      user_id: e.target.value
+      user_id:Object.assign(this.state.user_id, {
+        value: e.target.value
+      })  
     })
   }
 
@@ -130,16 +239,17 @@ class EmployeeAdd extends Component {
       <div style={{marginLeft : '1%', marginRight : '1%'}} >
       <h3 className='heading'>Add a Employee</h3>
         <Row>
-        <Input  onChange={this.handleUser_Id}s={6} value={this.state.user_id} label="* Employee Id" />
-          <Input  onChange={this.handleFirstname}s={6} value={this.state.first_name} label="* First Name" />
-          <Input  onChange={this.handleLastname} s={6} value={this.state.last_name} label="* Last Name" />
-          <Input type="number" min='0'label="* age"value={this.state.age} onChange={this.handleAge}s={6} />
-          <Input s={6} type='select' label="* gender"value={this.state.gender} onChange={this.handleGender}defaultValue='Other'>
+        <Input  onChange={this.handleUser_Id}s={6} value={this.state.user_id.value} label="* Employee Id"error={this.state.user_id.showError ? this.state.user_id.error : null} />
+          <Input  onChange={this.handleFirstname}s={6} value={this.state.first_name.value} label="* First Name" error={this.state.first_name.showError ? this.state.first_name.error : null}/>
+          <Input  onChange={this.handleLastname} s={6} value={this.state.last_name.value} label="* Last Name" error={this.state.last_name.showError ? this.state.last_name.error : null}/>
+          <Input type="number" min='0'label="* age"value={this.state.age.value} onChange={this.handleAge}s={6} error={this.state.age.showError ? this.state.age.error : null}/>
+          <Input s={6} type='select' label="* gender"value={this.state.gender.value} onChange={this.handleGender} error={this.state.gender.showError ? this.state.gender.error : null}>
             <option value='Male'>Male</option>
             <option value='Female'>Female</option>
             <option value='Other'>Other</option>
           </Input>
-          <Input s={6} type='select' label="* Department" defaultValue='HR'onChange={this.handleDepartment}>
+          <Input s={6} type='select' label="* Department" defaultValue='HR'onChange={this.handleDepartment} error={this.state.department.showError ? this.state.department.error : null}>
+            <option value='select'>select</option>
             <option value='HR'>HR</option>
             <option value='Delivery'>Delivery</option>
             <option value='Finance/Accounting'>Finance/Accounting</option>
@@ -147,7 +257,8 @@ class EmployeeAdd extends Component {
             <option value='Developer/Designer'>Developer/Designer</option>
             <option value='Testing'>Testing</option>
           </Input>
-          {this.state.department === 'Developer/Designer' ? <Input s={6} type='select' label="* Designation" defaultValue='Developer/Designer'onChange={this.handleDesignation}>
+          {this.state.department.value === 'Developer/Designer' ? <Input s={6} type='select' label="* Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+            <option value='select'>select</option>
             <option value='Team Lead'>Team Lead</option>
             <option value='Sr.Software Development Engineer'>Sr.Software Development Engineer</option>
             <option value='Software Development Engineer'>Software Development Engineer</option>
@@ -156,21 +267,25 @@ class EmployeeAdd extends Component {
             
             
           </Input> : null}
-          {this.state.department === 'HR' ? <Input s={6} type='select' label="* Designation" defaultValue='Sr.HR Manager'onChange={this.handleDesignation}>
+          {this.state.department.value === 'HR' ? <Input s={6} type='select' label="* Designation" defaultValue='Select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+            <option value='select'>select</option>
             <option value='Sr.HR Manager'>Sr.HR Manager</option>
             <option value='HR Recruitment Manager'>HR Recruitment Manager</option>
           </Input> : null }
-          {this.state.department === 'Delivery' ? <Input s={6} type='select' label="* Designation" defaultValue='Delivery Manager'onChange={this.handleDesignation}>
+          {this.state.department.value === 'Delivery' ? <Input s={6} type='select' label="* Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+            <option value='select'>select</option>
             <option value='Delivery Manager'>Delivery Manager</option>
             <option value='Sr.Project Manager'>Sr.Project Manager</option>
             <option value='Project Manager'>Project Manager</option>
           </Input> : null }
-          {this.state.department === 'Finance/Accounting' ?   <Input s={6} label="* Designation" defaultValue='Finance Director' /> : null }
-          {this.state.department === 'Pre sales' ? <Input s={6} type='select' label="* Designation" defaultValue='Lead Presales'onChange={this.handleDesignation}>
+          {this.state.department.value === 'Finance/Accounting' ?   <Input s={6} label="* Designation" defaultValue='Finance Direcr' /> : null }
+          {this.state.department.value === 'Pre sales' ? <Input s={6} type='select' label="* Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+            <option value='select'>select</option>
             <option value='Lead Presales'>Lead Presales</option>
             <option value='Presales Associate'>Presales Associate</option>
           </Input> : null }
-          {this.state.department === 'Testing' ? <Input s={6} type='select' label="* Designation" defaultValue='QA Lead'onChange={this.handleDesignation}>
+          {this.state.department.value === 'Testing' ? <Input s={6} type='select' label="* Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+            <option value='select'>select</option>
             <option value='QA Lead'>QA Lead</option>
             <option value='Software Test Development Engineer'>Software Test Development Engineer</option>
           </Input> : null}
--- src/EmployeeTicketsList.js
+++ src/EmployeeTicketsList.js
@@ -5,21 +5,26 @@ import moment from 'moment';
 import {Link} from 'react-router-dom'
 import './Employee.css'
 import './ListPage.css'
+import $ from 'jquery'
 
 
 class EmployeeTicketsList extends Component{
     constructor(props){
         super(props)
-        this.state = {
-            ticketsList:[],
-            pagination : {totalPage : 1, currentPage : 1},
-            page : 1,     
+        this.state = {   
+            assetPagination : {totalPage : 1, currentPage : 1},
+            assetPage : 1,
+            consumablePagination : {totalPage : 1, currentPage : 1},
+            consumablePage : 1, 
+            AssetsticketsList : [],
+            ConsumablesticketsList : [],    
             handleListRequest : true,
             isPendingChecked : true,
             isAcceptedChecked : false,
             isRejectedChecked : false,
         }
-        this.setPage = this.setPage.bind(this)
+        this.setAssetPage = this.setAssetPage.bind(this)
+        this.setConsumablePage = this.setConsumablePage.bind(this)
         this.setHandleListRequest = this.setHandleListRequest.bind(this)
         this.handleList = this.handleList.bind(this)
         this.setPendingChecked = this.setPendingChecked.bind(this)
@@ -27,19 +32,22 @@ class EmployeeTicketsList extends Component{
         this.setRejectedChecked = this.setRejectedChecked.bind(this)
     }
     setPendingChecked(){
-        this.setPage(1)
+        this.setAssetPage(1)
+        this.setConsumablePage(1)
         this.setState({
             isPendingChecked : !this.state.isPendingChecked,
         })
     }
     setAcceptedChecked(){
-        this.setPage(1)
+        this.setAssetPage(1)
+        this.setConsumablePage(1)
         this.setState({
             isAcceptedChecked : !this.state.isAcceptedChecked,
         })
     }
     setRejectedChecked(){
-        this.setPage(1)
+        this.setAssetPage(1)
+        this.setConsumablePage(1)
         this.setState({
             isRejectedChecked : !this.state.isRejectedChecked,
         })
@@ -50,9 +58,15 @@ class EmployeeTicketsList extends Component{
         })
 
     }
-    setPage(e){
+    setAssetPage(e){
         this.setState({
-            page : e,
+            assetpage : e,
+            handleListRequest : true
+        })
+    }
+    setConsumablePage(e){
+        this.setState({
+            consumablepage : e,
             handleListRequest : true
         })
     }
@@ -65,8 +79,10 @@ class EmployeeTicketsList extends Component{
             })
             .then(res => {
                 this.setState({
-                    ticketsList : res.data.ticketsListing,
-                    pagination : res.data.pagination,
+                    assetPagination : res.data.assetPagination,
+                    consumablePagination : res.data.consumablePagination,
+                    AssetsticketsList : res.data.ticketsAssetsListing,
+                    ConsumablesticketsList : res.data.ticketsConsumableListing,
                     handleListRequest : false
                 })
                 // if(this.state.ticketsList.length === 0){
@@ -85,8 +101,10 @@ class EmployeeTicketsList extends Component{
             })
             .then(res => {
                 this.setState({
-                    ticketsList : res.data.ticketsListing,
-                    pagination : res.data.pagination,
+                    assetPagination : res.data.assetPagination,
+                    consumablePagination : res.data.consumablePagination,
+                    AssetsticketsList : res.data.ticketsAssetsListing,
+                    ConsumablesticketsList : res.data.ticketsConsumableListing,
                     handleListRequest : false
                 })
                 // if(this.state.ticketsList.length === 0){
@@ -105,6 +123,7 @@ class EmployeeTicketsList extends Component{
             <div style={{marginLeft : '1%', marginRight : '1%'}}>
                 <h3 className='heading'>Employee Tickets List</h3>
                 {this.state.handleListRequest ? this.handleList() : null}
+
                 <div className="filterContainer">
                 <p style={{color:'white'}} className="adminDashboardTitle">Status Filters</p>
                     <Row className="ticketListCheckbox">
@@ -113,47 +132,169 @@ class EmployeeTicketsList extends Component{
                         <Input name='filter' type='checkbox' value='red' label='Rejected' onClick = {this.setRejectedChecked} checked={this.state.isRejectedChecked} />
                     </Row>
                 </div>
-                {this.state.ticketsList.length === 0 ? <h4 style={{left : '50%'}}>No Records</h4>:
-                <Table >
-                    <thead>
-                        <tr>
-                            <th data-field="ticket_number">Ticket Number</th>
-                            <th data-field="date">Requested Date</th>
-                            <th data-field="requestedItem">Requested Item</th>
-                            {/* <th data-field="requested_asset_id">Requested consumable item</th> */}
-                            {/* <th data-field="requested_consumable_id">Requested Consumable Id</th> */}
-                            {/* <th data-field="item_type">Resource Type</th> */}
-                            <th data-field="quantity">Quantity</th>
-                            <th data-field="status">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.ticketsList.map((ticket, index) => {
-                            return (<tr key={ticket.ticket_number}>
-                            <td>{ticket.ticket_number}</td>
-                            <td>{moment(ticket.date).format('DD - MM - YYYY')}</td>
-                            <td>{ticket.requested_asset_item ? `${ticket.requested_asset_item} [${ticket.item_type}]`: `${ticket.requested_consumable_item} [${ticket.item_type}]`}</td>
-                            {/* <td>{ticket.requested_consumable_item}</td> */}
-                            {/* <td>{ticket.item_type}</td> */}
-                            <td>{ticket.quantity}</td>
-                            <td>{ticket.status}</td>
-                    
-                            </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>}
-                <Link to={{ pathname : '/employeehomepage/RequestTicket'}}><Button style={{position : 'fixed'}} floating large className = 'red addVendorButton' waves = 'light' icon = 'add' /></Link>
+               
+               <Tabs className='tabs' tabHeaders={['Assets', 'Consumables']} selectedIndex={0}>
+                    <div className = "assetTab">
+                        {this.state. AssetsticketsList.length === 0 ? <div className="noRecordsScreen">No Asset Tickets</div> :
+                        <div>
+                            <Table style={{marginLeft:'1%'}}  className="consumableTable">
+                                <thead>
+                                    <tr>
+                                    <th data-field="ticket_number">Ticket Number</th>
+                                    <th data-field="date">Requested Date</th>
+                                    <th data-field="requestedItem">Requested Item</th>
+                                    <th data-field="quantity">Quantity</th>
+                                    <th data-field="status">Status</th>
+                                    </tr>
+                                </thead>
 
-           
-                <div>
-                    <Pagination items={this.state.pagination.totalPage} activePage={this.state.page} maxButtons={5} onSelect = {this.setPage} />
-                </div>
+                                <tbody>
+                                    {this.state.AssetsticketsList.map((ticket, index) => {
+                                        return (<tr key={ticket.ticket_number}>
+                                        <td>{ticket.ticket_number}</td>
+                                        <td>{moment(ticket.date).format('DD - MM - YYYY')}</td>
+                                        <td>{ticket.requested_asset_item ? `${ticket.requested_asset_item} [${ticket.item_type}]`: `${ticket.requested_consumable_item} [${ticket.item_type}]`}</td>
+                                        <td>{ticket.quantity}</td>
+                                        <td>{ticket.status}</td>
+                                
+                                        </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </Table>
+                            
+                            <Pagination items={this.state.assetPagination.totalPage} activePage={this.state.assetPage} maxButtons={5} onSelect = {this.setAssetPage} />
+                        </div>}
+                            <Link to={{ pathname : '/employeehomepage/RequestTicket'}}><Button style={{position : 'fixed'}} floating large className = 'red addVendorButton' waves = 'light' icon = 'add' /></Link>
+                    </div>
+
+                    <div className = "consumableTab">
+                        {this.state.ConsumablesticketsList.length === 0 ? <div className="noRecordsScreen">No Consumable Tickets</div> :
+                        <div>
+                            <Table style={{marginLeft:'1%'}}  className="consumableTable">
+                                <thead>
+                                    <tr>
+                                    <th data-field="ticket_number">Ticket Number</th>
+                                    <th data-field="date">Requested Date</th>
+                                    <th data-field="requestedItem">Requested Item</th>
+                                    <th data-field="quantity">Quantity</th>
+                                    <th data-field="status">Status</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {this.state.ConsumablesticketsList.map((ticket, index) => {
+                                        return (<tr key={ticket.ticket_number}>
+                                        <td>{ticket.ticket_number}</td>
+                                        <td>{moment(ticket.date).format('DD - MM - YYYY')}</td>
+                                        <td>{ticket.requested_asset_item ? `${ticket.requested_asset_item} [${ticket.item_type}]`: `${ticket.requested_consumable_item} [${ticket.item_type}]`}</td>
+                                        <td>{ticket.quantity}</td>
+                                        <td>{ticket.status}</td>
+                                
+                                        </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </Table>
+                            <Pagination items={this.state.consumablePagination.totalPage} activePage={this.state.consumablePage} maxButtons={5} onSelect = {this.setConsumablePage} />
+                        </div>}
+                            <Link to={{ pathname : '/employeehomepage/RequestTicket'}}><Button style={{position : 'fixed'}} floating large className = 'red addVendorButton' waves = 'light' icon = 'add' /></Link>
+                    </div>
+
+                    {/* <Tab title="" disabled></Tab> */}
+                </Tabs>
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+              
             </div>
         )
     }
     
 }
+class Tabs extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            selectedIndex: this.props.selectedIndex || 0,
+        }
+
+        this.setActiveTab = this.setActiveTab.bind(this);
+    }
+    setActiveTab(index) {
+        this.setState({selectedIndex: index})
+    }
+    render(){
+        return (<div>
+            <div style={{display : 'flex'}} className="tabs z-depth-1 flow-text">
+                {
+                    this.props.tabHeaders.map((tab, index) => <Tab onClick={this.setActiveTab} key={index} index={index}>{tab}</Tab>)
+                }
+            </div>
+            <div className="content">
+                {this.props.children[this.state.selectedIndex]}
+            </div>
+        </div>)
+    }
+}
+
+class Tab extends Component {
+    constructor (props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.onClick(this.props.index)
+        $(`.tabs div`).removeClass('activeTab')
+        $(`.tabs div:nth-child(${this.props.index+1})`).addClass('activeTab')
+    }
+    render(){
+        return <div style={{width : '30%', paddingLeft : '1%', cursor : 'pointer'}} onClick={this.handleClick}>{this.props.children}</div>
+    }
+}
+
 export default EmployeeTicketsList
 
 
+{/* <Table >
+<thead>
+    <tr>
+        <th data-field="ticket_number">Ticket Number</th>
+        <th data-field="date">Requested Date</th>
+        <th data-field="requestedItem">Requested Item</th>
+        <th data-field="quantity">Quantity</th>
+        <th data-field="status">Status</th>
+    </tr>
+</thead>
+<tbody>
+    {this.state.AssetsticketsList.map((ticket, index) => {
+        return (<tr key={ticket.ticket_number}>
+        <td>{ticket.ticket_number}</td>
+        <td>{moment(ticket.date).format('DD - MM - YYYY')}</td>
+        <td>{ticket.requested_asset_item ? `${ticket.requested_asset_item} [${ticket.item_type}]`: `${ticket.requested_consumable_item} [${ticket.item_type}]`}</td>
+        <td>{ticket.quantity}</td>
+        <td>{ticket.status}</td>
+
+        </tr>
+        )
+    })}
+</tbody>
+</Table>}
+<Link to={{ pathname : '/employeehomepage/RequestTicket'}}><Button style={{position : 'fixed'}} floating large className = 'red addVendorButton' waves = 'light' icon = 'add' /></Link>
+
+
+<div>
+<Pagination items={this.state.pagination.totalPage} activePage={this.state.page} maxButtons={5} onSelect = {this.setPage} />
+</div> */}
