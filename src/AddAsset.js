cbe7cd4e27071d8e95cceb090e34886f9ad4318e
EmmanuelCBaby <emmanuel.b@westagilelabs.com>
2018-04-18 14:27:18 +0530

    added validation to add asset and made the forgot password screen

--- src/AddAsset.js
+++ src/AddAsset.js
@@ -10,18 +10,54 @@ class AddAsset extends Component{
     constructor(props){
         super(props)
         this.state = {
-            serial_number : '',
-            asset_name : '',
-            purchase_date : '',
+            serial_number : {
+                value: '',
+                error: '',
+                showError: false
+            },
+            asset_name : {
+                value: '',
+                error: '',
+                showError: false
+            },
+            purchase_date : {
+                value: '',
+                error: '',
+                showError: false
+            },
             description : '',
-            invoice_number : '',
-            vendor : 'Select',
-            amount : 0,
-            gst : 0,
+            invoice_number : {
+                value: '',
+                error: '',
+                showError: false
+            },
+            vendor : {
+                value: 'Select',
+                error: '',
+                showError: false
+            },
+            amount : {
+                value: 0,
+                error: '',
+                showError: false
+            },
+            gst : {
+                value: 0,
+                error: '',
+                showError: false
+            },
             total : 0,
             category : 'Select',
-            condition : '',
-            location : '',
+            condition : {
+                value: '',
+                error: '',
+                showError: false
+            },
+            location : {
+                value: '',
+                error: '',
+                showError: false
+            },
             vendorList : [],
             assetTypeList : [],
             assetType : 'Select',
@@ -51,8 +87,104 @@ class AddAsset extends Component{
     }
 
     checkForValidation(){
-        if(!this.state.serial_number || !this.state.asset_name || !this.state.purchase_date || !this.state.invoice_number || this.state.vendor === 'Select' || !this.state.amount || !this.state.condition || !this.state.location || this.state.category ==='Select'){
-            window.Materialize.toast('All the * marked fields are required', 4000)
+        // if(!this.state.serial_number || !this.state.asset_name || !this.state.purchase_date || !this.state.invoice_number || this.state.vendor === 'Select' || !this.state.amount || !this.state.condition || !this.state.location || this.state.category ==='Select'){
+        //     window.Materialize.toast('All the * marked fields are required', 4000)
+        // }
+        if(!this.state.serial_number.value){
+            this.setState({
+                serial_number:Object.assign(this.state.serial_number, {
+                    error: 'The serial number is required',
+                    showError : true
+                })
+            })
+        }
+        if(!this.state.asset_name.value){
+            this.setState({
+                asset_name:Object.assign(this.state.asset_name, {
+                    error: 'The Asset name is required',
+                    showError : true
+                })
+            })
+        }
+        if(!this.state.purchase_date.value){
+            this.setState({
+                purchase_date:Object.assign(this.state.purchase_date, {
+                    error: 'The Purchase date is required',
+                    showError : true
+                })
+            })
+        }
+        if(!this.state.invoice_number.value){
+            this.setState({
+                invoice_number:Object.assign(this.state.invoice_number, {
+                    error: 'The Invoice number is required',
+                    showError : true
+                })
+            })
+        }
+        if(this.state.amount.value === 0){
+            this.setState({
+                amount:Object.assign(this.state.amount, {
+                    error: 'The Amount should not be zero.',
+                    showError : true
+                })
+            })
+        }
+        if(this.state.amount.value < 0){
+            this.setState({
+                amount:Object.assign(this.state.amount, {
+                    error: 'The Amount should not be neagtive.',
+                    showError : true
+                })
+            })
+        }
+        if(this.state.amount.value > 0){
+            this.setState({
+                amount:Object.assign(this.state.amount, {
+                    error: '',
+                    showError : false
+                })
+            })
+        }
+        if(this.state.gst < 0){
+            this.setState({
+                gst:Object.assign(this.state.gst, {
+                    error: 'The gst should not be negative',
+                    showError : true
+                })
+            })
+        }
+        if(this.state.gst > 0){
+            this.setState({
+                gst:Object.assign(this.state.gst, {
+                    error: '',
+                    showError : false
+                })
+            })
+        }
+        if(!this.state.condition.value){
+            this.setState({
+                condition:Object.assign(this.state.condition, {
+                    error: 'The condition of the asset is required',
+                    showError: true
+                })
+            })
+        }
+        if(!this.state.location.value){
+            this.setState({
+                location:Object.assign(this.state.location, {
+                    error:'The location is required',
+                    showError: true
+                })
+            })
+        }
+        if(this.state.vendor.value === 'Select'){
+            this.setState({
+                vendor:Object.assign(this.state.vendor, {
+                    error:'Select the Vendor',
+                    showError:true
+                })
+            })
         }
         else{
             this.setState({
@@ -70,19 +202,25 @@ class AddAsset extends Component{
 
     setSerialNumber(e){
         this.setState({
-            serial_number : e.target.value
+            serial_number : Object.assign(this.state.serial_number, {
+                value: e.target.value
+            })
         })
     }
 
     setAssetName(e){
         this.setState({
-            asset_name : e.target.value
+            asset_name : Object.assign(this.state.asset_name, {
+                value: e.target.value
+            })
         })
     }
 
     setPurchaseDate(e){
         this.setState({
-            purchase_date : e.target.value
+            purchase_date : Object.assign(this.state.purchase_date, {
+                value : e.target.value
+            })
         })
     }
 
@@ -94,25 +232,33 @@ class AddAsset extends Component{
 
     setInvoiceNumber(e){
         this.setState({
-            invoice_number : e.target.value
+            invoice_number : Object.assign(this.state.invoice_number, {
+                value : e.target.value
+            })
         })
     }
 
     setVendor(e){
         this.setState({
-            vendor : e.target.value
+            vendor : Object.assign(this.state.vendor, {
+                value : e.target.value
+            })
         })
     }
 
     setAmount(e){
         this.setState({
-            amount : Number(e.target.value),
+            amount : Object.assign(this.state.amount, {
+                value : Number(e.target.value)
+            })
         })
     }
 
     setGst(e){
         this.setState({
-            gst : Number(e.target.value),
+            gst : Object.assign(this.state.gst, {
+                value: Number(e.target.value)
+            })
         })
     }
 
@@ -124,13 +270,17 @@ class AddAsset extends Component{
 
     setCondition(e){
         this.setState({
-            condition : e.target.value
+            condition : Object.assign(this.state.condition, {
+                value: e.target.value
+            })
         })
     }
 
     setLocation(e){
         this.setState({
-            location : e.target.value
+            location : Object.assign(this.state.location, {
+                value: e.target.value
+            })
         })
     }
 
@@ -177,18 +327,18 @@ class AddAsset extends Component{
             url : 'http://localhost:3001/asset/create',
             withCredentials : true,
             data : {
-                serial_number : this.state.serial_number ,
-                asset_name : this.state.asset_name,
-                purchase_date : this.state.purchase_date,
+                serial_number : this.state.serial_number.value ,
+                asset_name : this.state.asset_name.value,
+                purchase_date : this.state.purchase_date.value,
                 description : this.state.description,
-                invoice_number : this.state.invoice_number,
-                vendor : this.state.vendor,
-                amount : this.state.amount,
-                gst : this.state.gst,
+                invoice_number : this.state.invoice_number.value,
+                vendor : this.state.vendor.value,
+                amount : this.state.amount.value,
+                gst : this.state.gst.value,
                 total : this.state.total,
                 category : this.state.category,
-                condition : this.state.condition,
-                location : this.state.location
+                condition : this.state.condition.value,
+                location : this.state.location.value
                 ,assetType : this.state.assetType
             }
         })
@@ -202,17 +352,53 @@ class AddAsset extends Component{
             else{
                 this.setState({
                     addAssetRequest : false,
-                    serial_number : '',
-                    asset_name : '',
-                    purchase_date : '',
+                    serial_number : {
+                        value: '',
+                        error: '',
+                        showError: false
+                    },
+                    asset_name : {
+                        value: '',
+                        error: '',
+                        showError: false
+                    },
+                    purchase_date : {
+                        value: '',
+                        error: '',
+                        showError: false
+                    },
                     description : '',
-                    invoice_number : '',
-                    vendor : 'Select',
+                    invoice_number : {
+                        value: '',
+                        error: '',
+                        showError: false
+                    },
+                    vendor : {
+                        value: 'Select',
+                        error: '',
+                        showError: false
+                    },
                     category : 'Select',
-                    condition : '',
-                    location : '',
-                    amount : 0,
-                    gst : 0,
+                    condition : {
+                        value: '',
+                        error: '',
+                        showError: false
+                    },
+                    location : {
+                        value: '',
+                        error: '',
+                        showError: false
+                    },
+                    amount : {
+                        value: 0,
+                        error: '',
+                        showError: false
+                    },
+                    gst : {
+                        value: 0,
+                        error: '',
+                        showError: false
+                    },
                     total : 0,
                     assetType : 'Select'
                 })
@@ -280,13 +466,13 @@ class AddAsset extends Component{
             <div style={{marginLeft : '1%', marginRight : '1%'}} >
                 <h3 className='heading'>Add Asset</h3>
                 <Row>
-                    <Input s={6} label="Serial Number *" value = {this.state.serial_number} onChange = {this.setSerialNumber} />
-                    <Input s={6} label="Asset Name *" value = {this.state.asset_name} onChange = {this.setAssetName} />
-                    <Input s={6} name='on' type='date' label="Purchased Date *" onChange={this.setPurchaseDate} value = {this.state.purchase_date} />
+                    <Input s={6} label="Serial Number *" value = {this.state.serial_number.value} onChange = {this.setSerialNumber} error={this.state.serial_number.showError ? this.state.serial_number.error : null}/>
+                    <Input s={6} label="Asset Name *" value = {this.state.asset_name.value} onChange = {this.setAssetName} error={this.state.asset_name.showError ? this.state.asset_name.error : null}/>
+                    <Input s={6} name='on' type='date' label="Purchased Date *" onChange={this.setPurchaseDate} value = {this.state.purchase_date.value} error={this.state.purchase_date.showError ? this.state.purchase_date.error : null} />
                     <Input s={6} label="Description" value = {this.state.description} onChange = {this.setDescription}/>
-                    <Input s={6} label="Invoice Number *" value = {this.state.invoice_number} onChange = {this.setInvoiceNumber}/>
-                    <Input s={6} label="Condition *" value = {this.state.condition} onChange = {this.setCondition}/>
-                    <Input s={6} label="Location *" value = {this.state.location} onChange = {this.setLocation}/>
+                    <Input s={6} label="Invoice Number *" value = {this.state.invoice_number.value} onChange = {this.setInvoiceNumber} error={this.state.invoice_number.showError ? this.state.invoice_number.error : null} />
+                    <Input s={6} label="Condition *" value = {this.state.condition.value} onChange = {this.setCondition} error={this.state.condition.showError ? this.state.condition.error : null} />
+                    <Input s={6} label="Location *" value = {this.state.location.value} onChange = {this.setLocation} error={this.state.location.showError ? this.state.location.error : null} />
                     <br />
                     <Input s={6} type='select' label="Category*" onChange = {this.setCategory} value={this.state.category}>
                         <option value='Select'>Select</option>
@@ -294,9 +480,9 @@ class AddAsset extends Component{
                         <option value='Non-Electronics'>Non-Electronics</option>
                         <option value='Other'>Other</option>
                     </Input>
-                    <Input s={6} label="Amount *" type = "number" min={0} value = {this.state.amount} onChange = {this.setAmount}/>
-                    <Input s={6} label="GST" type = "number" min={0} value = {this.state.gst} onChange = {this.setGst}/>
-                    <Input s={6} label="Vendor *" type='select' value={this.state.vendor} onChange = {this.setVendor}>{this.vendorListDropdown()}</Input>
+                    <Input s={6} label="Amount *" type = "number" min={0} value = {this.state.amount.value} onChange = {this.setAmount} error={this.state.amount.showError ? this.state.amount.error : null} />
+                    <Input s={6} label="GST" type = "number" min={0} value = {this.state.gst.value} onChange = {this.setGst} error={this.state.gst.showError ? this.state.gst.error : null} />
+                    <Input s={6} label="Vendor *" type='select' value={this.state.vendor.value} onChange = {this.setVendor} error={this.state.vendor.showError ? this.state.vendor.error :null} >{this.vendorListDropdown()}</Input>
                     <Input s={6} label="Asset Type*" type='select' value={this.state.assetType} onChange = {this.setAssetType}>{this.assetTypeDropdown()}</Input>
                     <Badge>Total : ₹{this.state.total.toFixed(2)}</Badge>
                     <br /> <br />
