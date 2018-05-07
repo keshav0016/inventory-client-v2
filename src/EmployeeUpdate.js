4b188ae1c82f0a28e389d555718de2488a92e29d
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-05-07 09:37:13 +0530

    changes made for employee update

--- src/EmployeeUpdate.js
+++ src/EmployeeUpdate.js
@@ -71,32 +71,37 @@ class EmployeeUpdate extends Component{
         $('label').addClass('active')
     }
    handleUpdate(){
-       axios({
-           method: 'post',
-           url: `${baseUrl}/employees/update`,
-           data:{
-               user_id: this.state.user_id,
-               first_name:this.state.first_name,
-                last_name:this.state.last_name,
-                age:this.state.age,
-                gender: this.state.gender,
-                department:this.state.department,
-                designation:this.state.designation
-           },
-           withCredentials: true
-       })
-       .then((res) => {
-        if(res.data.message === 'employee has been updated'){
-            window.Materialize.toast('Employee Edited', 4000)
-           this.props.setHandleListRequest()
-            
-        }
-          
-       })
-       .catch(error => {
-         window.Materialize.toast('can not edit employee', 4000)
-
-       })
+       if(this.props.user.department !== this.state.department && this.props.user.designation === this.state.designation){
+        window.Materialize.toast('if you change the department, designation should also be changed', 4000)
+       }else{
+        axios({
+            method: 'post',
+            url: `${baseUrl}/employees/update`,
+            data:{
+                user_id: this.state.user_id,
+                first_name:this.state.first_name,
+                 last_name:this.state.last_name,
+                 age:this.state.age,
+                 gender: this.state.gender,
+                 department:this.state.department,
+                 designation:this.state.designation
+            },
+            withCredentials: true
+        })
+        .then((res) => {
+         if(res.data.message === 'employee has been updated'){
+             window.Materialize.toast('Employee Edited', 4000)
+            this.props.setHandleListRequest()
+             
+         }
+           
+        })
+        .catch(error => {
+          window.Materialize.toast('can not edit employee', 4000)
+ 
+        })
+       }
+       
     }
     setFields(){
         this.setState({
@@ -135,6 +140,42 @@ class EmployeeUpdate extends Component{
                         
                     </Input>
                     <Input type="text"  defaultValue={this.state.designation}label="Current Designation"onChange={this.handleDesignation} s={6} disabled/>
+
+
+                    {this.state.department === 'Developer/Designer' ? <Input s={6} type='select' label="Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+                        <option value='select'>select</option>
+                        <option value='Team Lead'>Team Lead</option>
+                        <option value='Sr.Software Development Engineer'>Sr.Software Development Engineer</option>
+                        <option value='Software Development Engineer'>Software Development Engineer</option>
+                        <option value='Senior Product Designer'>Senior Product Designer</option>
+                        <option value='UI/UX Designer'>UI/UX Designer</option>
+                        
+                        
+                    </Input> : null}
+                    {this.state.department === 'HR' ? <Input s={6} type='select'  label="New Designation" defaultValue='Select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+                        <option value='select'>select</option>
+                        <option value='Sr.HR Manager'>Sr.HR Manager</option>
+                        <option value='HR Recruitment Manager'>HR Recruitment Manager</option>
+                    </Input> : null }
+                    {this.state.department === 'Delivery' ? <Input s={6} type='select'  label="New Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+                        <option value='select'>select</option>
+                        <option value='Delivery Manager'>Delivery Manager</option>
+                        <option value='Sr.Project Manager'>Sr.Project Manager</option>
+                        <option value='Project Manager'>Project Manager</option>
+                    </Input> : null }
+                    {this.state.department === 'Finance/Accounting' ?<Input s={6} type='select' label="New Designation" defaultValue='select' onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null} >
+                    <option value='select'>select</option>
+                    <option value='Finance Director'>Finance Director</option></Input>: null }
+                    {this.state.department === 'Pre sales' ? <Input s={6} type='select'  label="New Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+                        <option value='select'>select</option>
+                        <option value='Lead Presales'>Lead Presales</option>
+                        <option value='Presales Associate'>Presales Associate</option>
+                    </Input> : null }
+                    {this.state.department === 'Testing' ? <Input s={6} type='select'  label="New Designation" defaultValue='select'onChange={this.handleDesignation} error={this.state.designation.showError ? this.state.designation.error : null}>
+                        <option value='select'>select</option>
+                        <option value='QA Lead'>QA Lead</option>
+                        <option value='Software Test Development Engineer'>Software Test Development Engineer</option>
+                    </Input> : null}
                 </Row>
                  <Button onClick={this.handleUpdate}>Edit</Button>
                  <Button className="modal-close" onClick={this.setFields} style={{margin: '0 20px'}}>Cancel</Button>
