9aa3e0d20720e94c44632f5f3a5f5d877502adb3
Keshav Bohra <keshav.b@westagailelabs.com>
2018-05-21 18:30:54 +0530

    UI changes in the reset password

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -1,35 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import {Input, Button, Row, Icon, Col, Card} from 'react-materialize'
+import { Input, Button, Row, Icon, Col, Card } from 'react-materialize'
 import {
-   Redirect
-  } from 'react-router-dom';
+    Redirect
+} from 'react-router-dom';
 import './Employee.css'
 import $ from 'jquery'
 import { baseUrl } from './config';
+import logo from './LOGO.png'
 
 class ResetPassword extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            Confirm_Password : {
-                value:'',
-                error:'',
+            Confirm_Password: {
+                value: '',
+                error: '',
                 showError: false
             },
-            New_Password : {
-                value:'',
-                error:'',
+            New_Password: {
+                value: '',
+                error: '',
                 showError: false
             },
             user_id: this.props.match.params.user,
-            emailPassword : {
+            emailPassword: {
                 value: '',
                 error: '',
                 showError: false
             },
             change: true
-            ,employee : false
+            , employee: false
         }
         this.handleConfirmPassword = this.handleConfirmPassword.bind(this)
         this.handleNewPassword = this.handleNewPassword.bind(this)
@@ -38,26 +39,32 @@ class ResetPassword extends Component {
         this.handleUserid = this.handleUserid.bind(this)
         this.checkForValidation = this.checkForValidation.bind(this)
     }
-    render(){
-        var change =(
-            <div className='teal' style={{height: '100vh', position: 'relative'}}>
+    render() {
+        var change = (
+            <div className='white' style={{ height: '100vh', position: 'relative' }}>
+                <div className='header1' style={{ padding: 0, color: 'white', textAlign: 'center' }}>
+                    <Col s={2} offset={"m2"}>
+                        <img height='200px' width='200px' s={2} alt="wal" src={logo} />
+                    </Col>
+                    <h4 style={{ marginTop: 0, textAlign: 'center', fontWeight: 300, color: 'black' }}>Inventory Management System </h4>
+                </div>
                 <Row>
-                    <Col s={4} offset={'s4'} style={{marginTop:'90.67px'}}>
+                    <Col s={4} offset={'s4'} style={{ marginTop: 0 }}>
                         <form onSubmit={this.checkForValidation}>
-                            <Card className="z-depth-2" title="Reset your password" style={{padding: "30px"}}>
+                            <Card className="z-depth-2" title="Reset your password" style={{ padding: "30px" }}>
                                 <p>Check Your Email For Secret Password</p>
                                 <Row>
-                                    <Input autoFocus s={12} error={this.state.emailPassword.showError ? this.state.emailPassword.error : null}  label="Secret Password*" type='password' onChange={this.setEmailPassword} icon='lock' />
+                                    <Input autoFocus s={12} error={this.state.emailPassword.showError ? this.state.emailPassword.error : null} label="Secret Password*" type='password' onChange={this.setEmailPassword} icon='lock' />
                                 </Row>
                                 <Row>
-                                    <Input s={12} error={this.state.New_Password.showError ? this.state.New_Password.error : null}  type='password'onChange={this.handleNewPassword} label="New Password" ><Icon>lock</Icon></Input>
+                                    <Input s={12} error={this.state.New_Password.showError ? this.state.New_Password.error : null} type='password' onChange={this.handleNewPassword} label="New Password" ><Icon>lock</Icon></Input>
                                 </Row>
                                 <Row>
                                     <Input s={12} error={this.state.Confirm_Password.showError ? this.state.Confirm_Password.error : null} type='password' onChange={this.handleConfirmPassword} label="Confirm Password"><Icon>lock</Icon></Input>
                                 </Row>
                                 <Row>
                                     <Col offset={'s3'}>
-                                        <Button className='submitbtn'type="submit">Submit</Button>
+                                        <Button className='submitbtn' type="submit">Submit</Button>
                                     </Col>
                                 </Row>
                             </Card>
@@ -66,17 +73,17 @@ class ResetPassword extends Component {
                 </Row>
             </div>
         );
-        return ( 
+        return (
             <div>
                 {this.state.change ? change : null}
-                {this.state.employee ? (<Redirect push to = '/login'/>): null}
+                {this.state.employee ? (<Redirect push to='/login' />) : null}
             </div>
         )
     }
 
-    checkForValidation(e){
+    checkForValidation(e) {
         e.preventDefault()
-        if(!this.state.Confirm_Password.value){
+        if (!this.state.Confirm_Password.value) {
             this.setState({
                 Confirm_Password: Object.assign(this.state.Confirm_Password, {
                     error: 'The confirm password should not be empty',
@@ -84,7 +91,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(this.state.Confirm_Password.value){
+        if (this.state.Confirm_Password.value) {
             this.setState({
                 Confirm_Password: Object.assign(this.state.Confirm_Password, {
                     error: '',
@@ -92,7 +99,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(!this.state.New_Password.value){
+        if (!this.state.New_Password.value) {
             this.setState({
                 New_Password: Object.assign(this.state.New_Password, {
                     error: 'The new password should not be empty',
@@ -100,7 +107,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(this.state.New_Password.value){
+        if (this.state.New_Password.value) {
             this.setState({
                 New_Password: Object.assign(this.state.New_Password, {
                     error: '',
@@ -108,7 +115,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(!this.state.emailPassword.value){
+        if (!this.state.emailPassword.value) {
             this.setState({
                 emailPassword: Object.assign(this.state.emailPassword, {
                     error: 'The secret key should not be empty',
@@ -116,7 +123,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(this.state.emailPassword.value){
+        if (this.state.emailPassword.value) {
             this.setState({
                 emailPassword: Object.assign(this.state.emailPassword, {
                     error: '',
@@ -124,7 +131,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(this.state.New_Password.value !== this.state.Confirm_Password.value){
+        if (this.state.New_Password.value !== this.state.Confirm_Password.value) {
             this.setState({
                 Confirm_Password: Object.assign(this.state.Confirm_Password, {
                     error: 'The passwords does not match',
@@ -132,7 +139,7 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(this.state.New_Password.value === this.state.Confirm_Password.value){
+        if (this.state.New_Password.value === this.state.Confirm_Password.value) {
             this.setState({
                 Confirm_Password: Object.assign(this.state.Confirm_Password, {
                     error: '',
@@ -140,68 +147,68 @@ class ResetPassword extends Component {
                 })
             })
         }
-        if(this.state.Confirm_Password.value&&this.state.New_Password.value&&this.state.emailPassword.value&&this.state.New_Password.value === this.state.Confirm_Password.value){
+        if (this.state.Confirm_Password.value && this.state.New_Password.value && this.state.emailPassword.value && this.state.New_Password.value === this.state.Confirm_Password.value) {
             this.handleSubmit()
         }
     }
 
-    setEmailPassword(e){
+    setEmailPassword(e) {
         this.setState({
-            emailPassword : Object.assign(this.state.emailPassword, {
+            emailPassword: Object.assign(this.state.emailPassword, {
                 value: e.target.value
             })
         })
     }
 
-    handleUserid(e){
+    handleUserid(e) {
         this.setState({
-            user_id : Object.assign(this.state.user_id, {
+            user_id: Object.assign(this.state.user_id, {
                 value: e.target.value
             })
         })
     }
-    handleConfirmPassword(e){
+    handleConfirmPassword(e) {
         this.setState({
             Confirm_Password: Object.assign(this.state.Confirm_Password, {
                 value: e.target.value
             })
         })
     }
-    handleNewPassword(e){
+    handleNewPassword(e) {
         this.setState({
             New_Password: Object.assign(this.state.New_Password, {
                 value: e.target.value
             })
         })
     }
-    componentDidMount(){
+    componentDidMount() {
         $('label').addClass('active')
     }
-    handleSubmit(){
-        if(this.state.Confirm_Password.value === this.state.New_Password.value){
+    handleSubmit() {
+        if (this.state.Confirm_Password.value === this.state.New_Password.value) {
             axios({
                 method: 'post',
                 url: `${baseUrl}/employees/reset`,
                 data: {
                     user_id: this.state.user_id,
                     password: this.state.New_Password.value
-                    ,emailPassword : this.state.emailPassword.value
+                    , emailPassword: this.state.emailPassword.value
                 },
-                withCredentials : true
+                withCredentials: true
             })
-            .then((res) => {
-                if(res.data.message === 'password has been changed'){
-                    window.Materialize.toast('password has been changed',4000)
-                    this.setState({
-                        employee: true
-                    })
-                }
-            })
-        }else{
-            window.Materialize.toast('passwords does not match',4000)
+                .then((res) => {
+                    if (res.data.message === 'password has been changed') {
+                        window.Materialize.toast('password has been changed', 4000)
+                        this.setState({
+                            employee: true
+                        })
+                    }
+                })
+        } else {
+            window.Materialize.toast('passwords does not match', 4000)
         }
-       
+
     }
- 
+
 }
 export default ResetPassword
