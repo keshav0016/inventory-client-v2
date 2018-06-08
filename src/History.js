539b4e0f78870aa354ab6f0e98e461df690b7484
Keshav Bohra <keshav.b@westagailelabs.com>
2018-06-08 11:21:35 +0530

    reduced the asset detail heading

--- src/History.js
+++ src/History.js
@@ -81,10 +81,10 @@ class Assets extends Component{
                 <h3 className="title">Asset Details</h3>
                 {this.state.assetDetails ? <div>
                 {<div>
-                    {this.state.assetDetails.disabled === 1 ? <h4 className = "heading">Current Status : Unavailable </h4> : <React.Fragment>
-                {this.state.assetDetails.current_status === 'Available' ? <h4 className = "heading">Current Status : {this.state.assetDetails.current_status}</h4> : null }
-                {this.state.assetDetails.current_status === 'Assigned' ? <h4 className = "heading">Currently Assigned to {this.state.assignedEmployee !== null ?this.state.assignedEmployee.first_name+""+ this.state.assignedEmployee.last_name: <b style={{color:'teal'}}>Employee has left</b>} ({this.state.assignedEmployee !== null ?this.state.assignedEmployee.user_id: null})</h4> : null}
-                {this.state.assetDetails.current_status === 'Service' ? <h4 className = "heading">Currently under Service to {this.state.repairDetails.vendor} vendor and the Expected recovery is {moment(this.state.repairDetails.expected_delivery).format('DD MMM YYYY')}</h4> : null}
+                    {this.state.assetDetails.disabled === 1 ? <h5 className = "heading">Current Status : Unavailable </h5> : <React.Fragment>
+                {this.state.assetDetails.current_status === 'Available' ? <h5 className = "heading">Current Status : {this.state.assetDetails.current_status}</h5> : null }
+                {this.state.assetDetails.current_status === 'Assigned' ? <h5 className = "heading">Currently Assigned to {this.state.assignedEmployee !== null ?this.state.assignedEmployee.first_name+""+ this.state.assignedEmployee.last_name: <b style={{color:'teal'}}>Employee has left</b>} ({this.state.assignedEmployee !== null ?this.state.assignedEmployee.user_id: null})</h5> : null}
+                {this.state.assetDetails.current_status === 'Service' ? <h5 className = "heading">Currently under Service to {this.state.repairDetails.vendor} vendor and the Expected recovery is {moment(this.state.repairDetails.expected_delivery).format('DD MMM YYYY')}</h5> : null}
                 </React.Fragment>}
                 <Row className="splitModalButtons">
                     <a href='/admin/assets'><Button style={{float : 'left'}}>Go Back</Button></a>
